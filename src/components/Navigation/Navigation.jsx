69e6169d06e78ab55389cb55ca799cdb3ad33bf6
tiamga <167520641+tiamga@users.noreply.github.com>
2024-04-25 23:23:58 +0900

    User branch (#2)
     complete User

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,19 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { createUseStyles } from 'react-jss';
+
+import UserContext from '../User/User';
 
 const useStyles = createUseStyles({
   wrapper: {
-    borderBottom: 'black solid 1px',
+    outline: 'black solid 1px',
     padding: [15, 10],
     textAlign: 'right',
   }
 });
 
 export default function Navigation() {
+  const user = useContext(UserContext);
   const classes = useStyles();
   return(
     <div className={classes.wrapper}>
-      Welcome, Kwame
+      Welcome, {user.name}
     </div>
   )
 }
