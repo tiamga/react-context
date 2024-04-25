2cbc8a6d93455b879ca29baec3378873a6dca639
akatsukione <calvin168943@gmail.com>
2024-04-25 23:20:57 +0900

    complete User

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import Navigation from './components/Navigation/Navigation';
 import SaladMaker from './components/SaladMaker/SaladMaker';
+import UserContext from './components/User/User';
+
+const user = {
+  name: 'Kwame',
+  favorites: [
+    'avocado',
+    'carrot'
+  ]
+}
 
 function App() {
   return (
-    <>
+    <UserContext.Provider value={user}>
       <Navigation />
       <SaladMaker />
-    </>
+    </UserContext.Provider>
   );
 }
 
